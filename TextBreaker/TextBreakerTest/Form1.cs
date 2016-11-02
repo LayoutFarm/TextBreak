bf8ea6addeb83d94baffd83c7cd31961151090b2
win <wintercoredev@gmail.com>
2016-11-02 21:58:19 +0700

    05: very simple text breaker

---
+++ TextBreaker/TextBreakerTest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using LayoutFarm.TextBreaker;
+
+namespace TextBreakerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //break
+            EnTextBreaker enTextBreaker = new EnTextBreaker();
+            string text = this.textBox1.Text;
+            listBox1.Items.Clear();
+            char[] textBuffer = text.ToCharArray();
+            enTextBreaker.DoBreak(textBuffer, bounds =>
+            {
+                listBox1.Items.Add(
+                    new string(textBuffer, bounds.startIndex, bounds.length));
+
+
+            });
+        }
+    }
+}
